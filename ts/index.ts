f5d87d1cc5c036a5726ea64738ca32e405075080
minhduc5a12 <minhduc5a15@gmail.com>
2026-06-09 03:03:29 +0700

    Revert addon loading order for production

--- ts/index.ts
+++ ts/index.ts
@@ -8,26 +8,32 @@ let addon: any = null;
 const platformKey = `${platform()}-${arch()}`;
 
 try {
-  addon = require(join(__dirname, "..", "build", "Release", "codegraph_addon.node"));
-} catch (e1) {
+  switch (platformKey) {
+    case "darwin-arm64":
+      addon = require("@codegraph-x/core-darwin-arm64");
+      break;
+    case "darwin-x64":
+      addon = require("@codegraph-x/core-darwin-x64");
+      break;
+    case "linux-x64":
+      addon = require("@codegraph-x/core-linux-x64");
+      break;
+    case "win32-x64":
+      addon = require("@codegraph-x/core-win32-x64");
+      break;
+    default:
+      throw new Error(`Platform ${platformKey} is not supported.`);
+  }
+} catch (error) {
   try {
-    addon = require(join(__dirname, "..", "build", "codegraph_addon.node"));
-  } catch (e2) {
-    switch (platformKey) {
-      case "darwin-arm64":
-        addon = require("@codegraph-x/core-darwin-arm64");
-        break;
-      case "darwin-x64":
-        addon = require("@codegraph-x/core-darwin-x64");
-        break;
-      case "linux-x64":
-        addon = require("@codegraph-x/core-linux-x64");
-        break;
-      case "win32-x64":
-        addon = require("@codegraph-x/core-win32-x64");
-        break;
-      default:
-        throw new Error(`Platform ${platformKey} is not supported.`);
+    addon = require(join(__dirname, "..", "build", "Release", "codegraph_addon.node"));
+  } catch (e1) {
+    try {
+      addon = require(join(__dirname, "..", "build", "codegraph_addon.node"));
+    } catch (e2) {
+      throw new Error(
+        `Codegraph-X: Failed to load pre-compiled binary or local build for platform ${platformKey}. ${error}`,
+      );
     }
   }
 }
